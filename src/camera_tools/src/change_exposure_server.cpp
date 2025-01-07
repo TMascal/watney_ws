c24957c4e5b9a9efbe3638d1b1cea841ddc9609f
TMascal <mascalt@my.erau.edu>
2025-01-07 16:35:59 -0500

    Successful compile with custom interfaces

--- src/camera_tools/src/change_exposure_server.cpp
+++ src/camera_tools/src/change_exposure_server.cpp
@@ -3,20 +3,18 @@
 //
 
 #include "rclcpp/rclcpp.hpp"
-#include "camera_tools/srv/change_exposure.hpp"
-
+#include "camera_tools_interfaces/srv/change_exposure.hpp"
 #include <memory>
 
-void changeExposure(const std::shared_ptr<camera_tools::srv::ChangeExposure::Request> request,
-          std::shared_ptr<camera_tools::srv::ChangeExposure::Response> response)
+void changeExposure(const std::shared_ptr<camera_tools_interfaces::srv::ChangeExposure::Request> request,
+          std::shared_ptr<camera_tools_interfaces::srv::ChangeExposure::Response> response)
 {
 
-    // response->success = request->exposure_value;
+    response->success = request->exposure_value;
     response->success = true;
 
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Incoming request\na: %ld" " b: %ld",
-                  request->a, request->b);
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "sending back response: [%ld]", (long int)response->sum);
+    // RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Incoming request\na: %ld" " b: %ld", request->a, request->b);
+    // RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "sending back response: [%ld]", (long int)response->sum);
 }
 
 int main(int argc, char **argv)
@@ -25,8 +23,8 @@ int main(int argc, char **argv)
 
     std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("change_exposure_server");
 
-    rclcpp::Service<camera_tools::srv::ChangeExposure>::SharedPtr service =
-      node->create_service<camera_tools::srv::ChangeExposure>("change_exposure", &changeExposure);
+    rclcpp::Service<camera_tools_interfaces::srv::ChangeExposure>::SharedPtr service =
+      node->create_service<camera_tools_interfaces::srv::ChangeExposure>("change_exposure", &changeExposure);
 
     RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Ready to recieve command");
 
