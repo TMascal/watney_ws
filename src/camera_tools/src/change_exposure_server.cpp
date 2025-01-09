c81f38b8089267af9876dc9f404ec66947eccaaf
TMascal <mascalt@my.erau.edu>
2025-01-08 19:15:39 -0500

    Working ROS Service to Change Camera Exposure

--- src/camera_tools/src/change_exposure_server.cpp
+++ src/camera_tools/src/change_exposure_server.cpp
@@ -5,13 +5,44 @@
 #include "rclcpp/rclcpp.hpp"
 #include "camera_tools_interfaces/srv/change_exposure.hpp"
 #include <memory>
+#include <sstream>
 
 void changeExposure(const std::shared_ptr<camera_tools_interfaces::srv::ChangeExposure::Request> request,
           std::shared_ptr<camera_tools_interfaces::srv::ChangeExposure::Response> response)
 {
 
-    response->success = request->exposure_value;
+    // Inputs
+    int new_exposure_value = request->exposure_value;
     response->success = true;
+
+    // Video Device
+    int video_device = 2;
+
+    // Define Command Variable
+    std::stringstream enable_command_stream;
+    enable_command_stream << "v4l2-ctl -d /dev/video" << video_device << " -c auto_exposure=1" ; // index 1 sets exposure mode to manual
+    std::string enable_command = enable_command_stream.str();
+
+    // Execute Command and recored response
+    int enable_returnCode = std::system(enable_command.c_str());
+
+    RCLCPP_INFO(rclcpp::get_logger("change_exposure"), "Command executed with return code: %d", enable_returnCode);
+
+    // Define Command Variable
+    std::stringstream command_stream;
+    command_stream << "v4l2-ctl -d /dev/video" << video_device << " -c exposure_time_absolute=" << new_exposure_value;
+    std::string command = command_stream.str();
+
+    // Execute Command and recored response
+    int returnCode = std::system(command.c_str());
+
+    // Check if the command was executed successfully
+    if (returnCode == 0) {
+        RCLCPP_INFO(rclcpp::get_logger("change_exposure"), "Command executed successfully, exposure set to: %d", new_exposure_value);
+    } else {
+        RCLCPP_ERROR(rclcpp::get_logger("change_exposure"), "Command failed with return code: %d", returnCode);
+    }
+
 
     // RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Incoming request\na: %ld" " b: %ld", request->a, request->b);
     // RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "sending back response: [%ld]", (long int)response->sum);
