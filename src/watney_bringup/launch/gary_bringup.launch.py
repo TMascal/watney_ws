b65b640785c577665b173124155541a76ee4e313
jake161 <62108812+jake161@users.noreply.github.com>
2025-02-01 00:45:01 -0500

    Initial Launch FIle (Gary)

---
+++ src/watney_bringup/launch/gary_bringup.launch.py
@@ -0,0 +1,20 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch_ros.substitutions import FindPackageShare
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='high_to_low',
+            executable='serial_json',
+            name='h2l_node',
+            output='screen',
+            parameters=[{'port': '/dev/serial/by-id/usb-Arduino__www.arduino.cc__0043_75834303438351111041-if00'}]
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(FindPackageShare('ldlidar_node').find('ldlidar_node') + '/launch/ldlidar_with_mgr.launch.py'),
+            # launch_arguments={'arg_name': 'arg_value'}.items()  # Pass arguments to the included launch file
+        ),
+    ])
