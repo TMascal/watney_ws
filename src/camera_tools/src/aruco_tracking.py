2a77c1cec60e3c23430248dd0653246e0bf83038
Danielgolan0925 <136190206+Danielgolan0925@users.noreply.github.com>
2025-02-04 16:22:49 -0500

    Add files via upload

--- src/camera_tools/src/aruco_tracking.py
+++ src/camera_tools/src/aruco_tracking.py
@@ -1,7 +1,6 @@
 import cv2
 import numpy as np
 
-
 dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
 parameters = cv2.aruco.DetectorParameters()
 K = np.array([[1000, 0, 320], [0, 1000, 240], [0, 0, 1]])  # Update K-matrix later
@@ -21,8 +20,10 @@ while cap.isOpened():
             cv2.aruco.drawDetectedMarkers(frame, corners, ids)
             rvec, tvec, _ = cv2.aruco.estimatePoseSingleMarkers(marker_corners, 0.05, K, dist_coeffs)
             cv2.drawFrameAxes(frame, K, dist_coeffs, rvec[0], tvec[0], 0.03)
+            distance = np.linalg.norm(tvec[0][0])
             cX, cY = int(marker_corners[0][0][0]), int(marker_corners[0][0][1])
-            cv2.putText(frame, f"Frame {ids[i][0]}", (cX, cY - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+            cv2.putText(frame, f"ID: {ids[i][0]} Dist: {distance:.2f}m", (cX, cY - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+    
     cv2.imshow('ArUco Tracker', frame)
     
     if cv2.waitKey(1) & 0xFF == ord('q'):
