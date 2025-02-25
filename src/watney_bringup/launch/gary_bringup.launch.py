16b527cf4e3cad4a72e9b75a26980ab315b235a2
jake161 <62108812+jake161@users.noreply.github.com>
2025-02-25 11:42:35 -0500

    removed remappings

--- src/watney_bringup/launch/gary_bringup.launch.py
+++ src/watney_bringup/launch/gary_bringup.launch.py
@@ -18,8 +18,7 @@ def generate_launch_description():
             executable='imu_filter_madgwick_node',
             name='imu_filter_node',
             output='screen',
-            parameters=[{'use_mag': False}],
-            remappings=[('/imu/data_raw','/h2l_node/imu/raw'),('/imu/mag','/h2l_node/imu/mag')]
+            parameters=[{'use_mag': False}]
         ),
         Node(
             package='robot_localization',
