cfd79c5bd385fec7c6b6214fbdf30e31a009337b
jake161 <62108812+jake161@users.noreply.github.com>
2025-03-07 14:38:31 -0500

    Take 2

--- src/watney_bringup/launch/gary.launch.py
+++ src/watney_bringup/launch/gary.launch.py
@@ -2,11 +2,10 @@ import os
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, RegisterEventHandler
 from launch_ros.actions import Node
-from launch.event_handlers import OnProcessStart
+from launch.event_handlers import OnProcessExit
 from ament_index_python.packages import get_package_share_directory
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
-
 
 def generate_launch_description():
 
@@ -73,30 +72,30 @@ def generate_launch_description():
 
     # Add event handlers to launch nodes sequentially
     ld.add_action(RegisterEventHandler(
-        OnProcessStart(
+        OnProcessExit(
             target_action=high2low_node,
-            on_start=[imu_filter_node]
+            on_exit=[imu_filter_node]
         )
     ))
 
     ld.add_action(RegisterEventHandler(
-        OnProcessStart(
+        OnProcessExit(
             target_action=imu_filter_node,
-            on_start=[robot_localizaton_node]
+            on_exit=[robot_localizaton_node]
         )
     ))
 
     ld.add_action(RegisterEventHandler(
-        OnProcessStart(
+        OnProcessExit(
             target_action=robot_localizaton_node,
-            on_start=[ldlidar_slam_launch]
+            on_exit=[ldlidar_slam_launch]
         )
     ))
 
     ld.add_action(RegisterEventHandler(
-        OnProcessStart(
+        OnProcessExit(
             target_action=ldlidar_slam_launch,
-            on_start=[lidar_throttle_node]
+            on_exit=[lidar_throttle_node]
         )
     ))
 
