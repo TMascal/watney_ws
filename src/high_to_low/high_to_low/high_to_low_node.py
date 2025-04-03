d9e1c3f714e90acf1160817b147a4b5e9c576b60
jake161 <62108812+jake161@users.noreply.github.com>
2025-04-03 18:50:54 -0400

    attempting a quick refactor

--- src/high_to_low/high_to_low/high_to_low_node.py
+++ src/high_to_low/high_to_low/high_to_low_node.py
@@ -192,8 +192,8 @@ class SerialNode(Node):
 
         lVel = float(json_data.get('L', 0.0))
         rVel = float(json_data.get('R', 0.0))
-        odl = float(json_data.get('odl', 0.0))/ppr * 2.0 * math.pi # Convert to Rad
-        odr = float(json_data.get('odr', 0.0))/ppr * 2.0 * math.pi # Convert to Rad
+        odl = float(json_data.get('odl', 0.0)) / ppr
+        odr = float(json_data.get('odr', 0.0)) / ppr
 
         delta_odl = odl - self.previous_odl
         delta_odr = odr - self.previous_odr
@@ -210,7 +210,7 @@ class SerialNode(Node):
         # if self.theta > math.pi:
         #     self.theta -= 2 * math.pi
 
-        delta_x = wheel_diameter / 2.0 * ((delta_odr + delta_odl) / 2)
+        delta_x = math.pi * wheel_diameter * ((delta_odr + delta_odl) / 2.0)
         delta_y = delta_x * math.sin(self.theta + (delta_odr + delta_odl) / width)
 
         self.x_position += delta_x
